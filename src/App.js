f9b355b5269765f5faad05d134be2c1a7c3a78ad
mattmallow <matthewenzo@icloud.com>
2021-04-02 14:05:20 +1300

    Update code according to Airbnb standard

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from'react';
+import React, { Component } from 'react'
 
-class App extends Component{
+class App extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       count: 0,
     }
   }
-  
-  makeIncrementer = amount => () =>
-    this.setState(prevState => ({
+
+  makeIncrementer = (amount) => () =>
+    this.setState((prevState) => ({
       count: prevState.count + amount,
-      }));
-  
-      increment = this.makeIncrementer(1);
-  
+    }))
+
+  // eslint-disable-next-line react/sort-comp
+  increment = this.makeIncrementer(1)
+
   render() {
-    return(
+    return (
       <div>
+        {/* eslint-disable-next-line react/destructuring-assignment */}
         <p>Count: {this.state.count}</p>
-        <button className="increment"onClick={this.increment}>Increment count</button>
+        {/* eslint-disable-next-line react/button-has-type */}
+        <button className="increment" onClick={this.increment}>
+          Increment count
+        </button>
       </div>
     )
   }
 }
-  
-export default App;
+
+export default App
